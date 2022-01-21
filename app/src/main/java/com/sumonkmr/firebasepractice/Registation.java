ab2c0266485d0c67d68c28f76c2c86ea47ff5cf8
sumonkmr <sumonkmr.dev@gmail.com>
2022-01-21 13:05:08 +0600

    add firebase authentication in the project!

--- app/src/main/java/com/sumonkmr/firebasepractice/Registation.java
+++ app/src/main/java/com/sumonkmr/firebasepractice/Registation.java
@@ -50,7 +50,7 @@ public class Registation extends AppCompatActivity {
             if (task.isSuccessful()){
             Toast.makeText(Registation.this,"Registration Successfully ",Toast.LENGTH_SHORT).show();
         }else {
-            Toast.makeText(Registation.this, "Try Again!!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Registation.this, "Something wrong...Try Again!!", Toast.LENGTH_SHORT).show();
         }
         });
 
